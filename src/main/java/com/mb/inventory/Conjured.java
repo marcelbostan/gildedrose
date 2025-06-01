5fc2f04e51e8661833d7e820280775a1c0ddebb6
marcelbostan <bostanmarcel@gmail.com>
2025-06-01 13:03:48 +0200

    style: add spacing for readability

--- src/main/java/com/mb/inventory/Conjured.java
+++ src/main/java/com/mb/inventory/Conjured.java
@@ -3,13 +3,17 @@ package com.mb.inventory;
 import com.mb.Item;
 
 public final class Conjured implements InventoryItemType {
+    
     @Override
     public Item update(Item item) {
         item = item.decreaseSellIn();
+        
         item = item.decreaseQualityByTwo();
+        
         if (item.isExpired()) {
             item = item.decreaseQualityByTwo();
         }
+        
         return item;
     }
 }
